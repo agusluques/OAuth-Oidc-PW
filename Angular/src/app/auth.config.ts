184e5ecad77eaae333d500b8c717926594034d3e
Agustin Luques <luquesagustin@gmail.com>
2020-09-07 17:44:41 -0300

    edit angular project

--- Angular/src/app/auth.config.ts
+++ Angular/src/app/auth.config.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'http://localhost:8080/auth/realms/Demo',
+    issuer: 'http://localhost:8080/auth/realms/PW',
     redirectUri: window.location.origin,
     clientId: 'angular-demo-app',
     responseType: 'code',
